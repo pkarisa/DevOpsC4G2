34ae32acf306f0b1f8680696e0ed7964c8b22af6
Mark Carlton <mcarlton33@gmail.com>
2022-12-16 10:16:46 +0300

    changes

--- Week-4/file-system.js
+++ Week-4/file-system.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 // Synchronous vs Asynchronous jobs
 
 // Synchronous === Blocking
-const fileData = fs.readFileSync('public/demo.html');
-console.log('File', fileData.toString());
+// const fileData = fs.readFileSync('public/demo.html');
+// console.log('File', fileData.toString());
 
 // Asynchronous === Non-Blocking
-// fs.readFile('public/demo.html', function(error, fileDataAsync) {
-//     if (error) {
-//         console.log('Error', error);
-//     } else {
-//         console.log('File', fileDataAsync.toString());
-//     }
-// });
+fs.readFile('public/demo.html', function(error, fileDataAsync) {
+    if (error) {
+        console.log('Error', error);
+    } else {
+        console.log('File', fileDataAsync.toString());
+    }
+});
 
 console.log('The file system demo has ended');

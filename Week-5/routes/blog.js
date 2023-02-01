efd437b8f25dfaa71698966e34a8bb1339c8ae66
Mark Carlton <mcarlton33@gmail.com>
2023-02-01 10:04:01 +0300

    connect nodejs app to mongodb

---
+++ Week-5/routes/blog.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const blogModel = require('./../models/blog');
+
+const router = express.Router();
+
+// CRUD Operations
+router.post('/', async function (req, res) {
+    const data = new blogModel({
+        title: req.body.title,
+        description: req.body.description,
+    });
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.get('/', async function (req, res) {
+    try {
+        const data = await blogModel.find();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.patch('/:id', async function (req, res) {
+    const id = req.params.id;
+    const updatedData = req.body;
+
+    try {
+        const dataUpdate = await blogModel.findByIdAndUpdate(id, updatedData, { new: true });
+        res.status(200).json(dataUpdate);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.delete('/:id', async function (req, res) {
+    const id = req.params.id;
+    try {
+        const dataDelete = await blogModel.findByIdAndDelete(id);
+        res.status(200).json(dataDelete);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+module.exports = router;
